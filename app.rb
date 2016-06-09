feec62172e3d8c7413d7e4594b9ad628705b2bfb
stve <steve.agalloco@gmail.com>
2016-06-09 00:22:52 -0400

    support tweet urls and detect embeds

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'omniauth'
 require 'omniauth-google-oauth2'
 require 'yt'
 require 'addressable/uri'
+require 'twitter'
+require 'unwind'
+require 'nokogiri'
 require_relative 'lib/credentials'
 
 class App < Sinatra::Base
@@ -40,20 +43,56 @@ class App < Sinatra::Base
       else
         'OOPS: Invalid URL'
       end
+    rescue Yt::Errors::RequestError => e
+      if e.reasons && e.reasons.include?('videoAlreadyInPlaylist')
+        'OK'
+      else
+        "OOPS: #{e.response_body['error']['message']}"
+      end
     rescue => e
-      "OOPS: #{e}"
+      "OOPS: #{e.message}"
     end
   end
 
   helpers do
     def extract(url)
-      uri = Addressable::URI.parse(url)
+      logger.debug "extract=#{url}"
+      final_url = follow(url)
 
-      if url =~ /youtube\.com/
+      logger.debug "final=#{final_url}"
+
+      if final_url =~ /youtube\.com/
+        uri = Addressable::URI.parse(final_url)
         uri.query_values['v']
-      elsif url =~ /youtu\.be/
-        uri = Addressable::URI.parse(url)
+      elsif final_url =~ /youtu\.be/
+        uri = Addressable::URI.parse(final_url)
         uri.path[1..-1]
+      elsif final_url =~ /twitter\.com/
+        embedded_tweet = twitter_client.oembed(final_url, omit_script: true)
+        content_url = extract_from_embedded_tweet(embedded_tweet.html)
+        extract(content_url)
+      end
+    end
+
+    def follow(url)
+      follower = Unwind::RedirectFollower.new(url)
+      follower.resolve
+      follower.final_url
+    end
+
+    def extract_from_embedded_tweet(tweet_body)
+      html_doc = Nokogiri::HTML(tweet_body)
+      html_doc.css('a').each do |link|
+        return link['href'].to_s if link['href'].to_s =~ /t\.co/
+      end
+    end
+
+    def twitter_client
+      @client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
       end
     end
   end
